576d310775a9ea9ae84366e5988ae7cb72e0eef7
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2024-04-13 15:20:59 +0300

    refactor(Pretty): print file line better

--- src/Utility/Pretty.hs
+++ src/Utility/Pretty.hs
@@ -2,15 +2,16 @@ module Utility.Pretty (printSection, printFileSection, attention) where
 
 import Data.Char (toUpper)
 import qualified System.Console.Terminal.Size as Console
+import Text.Parsec.Pos (SourcePos, sourceColumn, sourceLine, sourceName)
 import Utility.Fun ((|>))
 import Utility.Strings (ul, (+-+), (+\\+))
 
-printFileSection :: String -> String -> String -> String -> Int -> Int -> IO ()
-printFileSection name source title message row column = printSection title body
+printFileSection :: String -> SourcePos -> String -> String -> IO ()
+printFileSection source pos title message = printSection title body
   where
     body = "\n" ++ file +\\+ extract +\\+ message
-    file = "in" +-+ name
-    extract = attention source row column
+    file = "in" +-+ sourceName pos
+    extract = attention source pos
 
 printSection :: String -> String -> IO ()
 printSection title body = do
@@ -26,12 +27,14 @@ br width_ message = if message == "" then line else left +-+ message +-+ right
     dash = repeat '-'
 
 -- | Have to decide whether (row:column) are 0 or 1 indexed
-attention :: String -> Int -> Int -> String
-attention sourceText row column = "\n" ++ ul [line, tick]
+attention :: String -> SourcePos -> String
+attention sourceText pos = "\n" ++ ul [line, tick]
   where
     line = ln ++ (sourceText |> lines |> (!! row))
     tick = repeat ' ' |> take (length ln + column) |> (++ "^")
     ln = show row +-+ "| "
+    row = sourceLine pos
+    column = sourceColumn pos
 
 consoleWidth :: IO Int
 consoleWidth = do
